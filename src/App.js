d0b2a805d3f7c87106f9584f1e54d1b365ddbc62
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-10-17 14:57:36 +0200

    Added stopwatch functionality to buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
     <>
       <h1>Stopwatch</h1>
       <div>
-        <span>{"0" + Math.floor((time / 60000) % 60)}:</span>
-        <span>{"0" + Math.floor((time / 1000) % 60)}:</span>
-        <span>{"0" + ((time / 10) % 100)}</span>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </div>
       <div>
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        <button onClick={() => setRunning(true)}>Start</button>
+        <button onClick={() => setRunning(false)}>Stop</button>
+        <button onClick={() => setTime(0)}>Reset</button>
       </div>
     </>
   );
